9e23e4c590a9bed274bafc4fdde55ea42922a2f2
Rena Suzuki Wagner <rena.suzuki.wagner@gmail.com>
2021-04-13 16:38:11 +0200

    Put authmiddleware back

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ if (process.env.DELAY) {
 // Routes
 app.use("/", authRouter);
 app.use("/public", publicstyleRouter);
-app.use("/user", userRouter);
+app.use("/user", authMiddleware, userRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
